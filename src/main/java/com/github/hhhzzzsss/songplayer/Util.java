83733f6e7df3ce2f23a2e84841e63b77350fd05b
IllagerCaptain <76600137+IllagerCaptain@users.noreply.github.com>
2023-06-22 09:12:24 -0500

    Fix conflicts

--- src/main/java/com/github/hhhzzzsss/songplayer/Util.java
+++ src/main/java/com/github/hhhzzzsss/songplayer/Util.java
@@ -12,7 +12,9 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +30,50 @@ public class Util {
             Files.createDirectories(path);
         }
         catch (IOException e) {}
+    }
+
+    public static Path resolveWithIOException(Path path, String other) throws IOException {
+        try {
+            return path.resolve(other);
+        }
+        catch (InvalidPathException e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+
+    public static class LimitedSizeInputStream extends InputStream {
+        private final InputStream original;
+        private final long maxSize;
+        private long total;
+
+        public LimitedSizeInputStream(InputStream original, long maxSize) {
+            this.original = original;
+            this.maxSize = maxSize;
+        }
+
+        @Override
+        public int read() throws IOException {
+            int i = original.read();
+            if (i>=0) incrementCounter(1);
+            return i;
+        }
+
+        @Override
+        public int read(byte b[]) throws IOException {
+            return read(b, 0, b.length);
+        }
+
+        @Override
+        public int read(byte b[], int off, int len) throws IOException {
+            int i = original.read(b, off, len);
+            if (i>=0) incrementCounter(i);
+            return i;
+        }
+
+        private void incrementCounter(int size) throws IOException {
+            total += size;
+            if (total>maxSize) throw new IOException("Input stream exceeded maximum size of " + maxSize + " bytes");
+        }
     }
 
     public static String formatTime(long milliseconds) {
@@ -77,7 +123,12 @@ public class Util {
         Path dir = SongPlayer.SONG_DIR;
         if (lastSlash >= 0) {
             dirString = arg.substring(0, lastSlash+1);
-            dir = dir.resolve(dirString);
+            try {
+                dir = resolveWithIOException(dir, dirString);
+            }
+            catch (IOException e) {
+                return null;
+            }
         }
 
         Stream<Path> songFiles;
@@ -130,7 +181,12 @@ public class Util {
         Path dir = SongPlayer.SONG_DIR;
         if (lastSlash >= 0) {
             dirString = arg.substring(0, lastSlash+1);
-            dir = dir.resolve(dirString);
+            try {
+                dir = resolveWithIOException(dir, dirString);
+            }
+            catch (IOException e) {
+                return null;
+            }
         }
         else {
             dirString = "";
