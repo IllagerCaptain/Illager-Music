f651312184ec51a5f1e3781528ee8ee20ac68185
IllagerCaptain <76600137+IllagerCaptain@users.noreply.github.com>
2023-09-15 19:46:25 -0500

    Change commands to /nickmm to work with TotalFreedom Reborn

--- src/main/java/com/github/hhhzzzsss/songplayer/playing/SongHandler.java
+++ src/main/java/com/github/hhhzzzsss/songplayer/playing/SongHandler.java
@@ -272,21 +272,21 @@ public class SongHandler {
             stage.sendMovementPacketToStagePosition();
             SongPlayer.addChatMessage("§6Now playing §3" + currentSong.name);
             if (currentPlaylist != null && currentPlaylist.name == "Default") {
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nick &b&lIllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <bold><aqua>lIllagerCaptain</aqua></bold>");
                         SongPlayer.MC.getNetworkHandler().sendCommand("me &b&lis now playing &6♪ &o&l" + currentSong.name + "&6 ♪&b&l at &9&l/warp illager_music&b&l!");
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nickgradient #ff0000 #0000ff IllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <gradient:#ff0000:#0000ff>IllagerCaptain</gradient>");
             } else if (currentPlaylist != null && currentPlaylist.name == "Christmas") {
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nick &c&lIllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <bold><red>lIllagerCaptain</red></bold>");
                         SongPlayer.MC.getNetworkHandler().sendCommand("me &2&lis &c&lnow &2&lplaying &6♪ &o&l" + currentSong.name + "&6 ♪&c&l &2&lat &9&l/warp illager_music&c&l!");
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nickgradient #ff5555 #00aa00 IllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <gradient:#ff5555:#00aa00>IllagerCaptain</gradient>");
             } else if (currentPlaylist != null && currentPlaylist.name == "Halloween") {
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nick &b&lIllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <bold><aqua>lIllagerCaptain</aqua></bold>");
                         SongPlayer.MC.getNetworkHandler().sendCommand("me &b&lis now playing &6♪ &o&l" + currentSong.name + "&6 ♪&b&l at &9&l/warp illager_music&b&l!");
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nickgradient #ffaa00 #00aa00 IllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <gradient:#ffaa00:#00aa00>IllagerCaptain</gradient>");
             } else if (currentPlaylist != null) {
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nick &b&lIllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <bold><aqua>lIllagerCaptain</aqua></bold>");
                         SongPlayer.MC.getNetworkHandler().sendCommand("me &b&lis now playing &6♪ &o&l" + currentSong.name + "&6 ♪&b&l at &9&l/warp illager_music&b&l!");
-                        SongPlayer.MC.getNetworkHandler().sendCommand("nickgradient #ff0000 #0000ff IllagerCaptain");
+                        SongPlayer.MC.getNetworkHandler().sendCommand("nickmm <gradient:#ff0000:#0000ff>IllagerCaptain</gradient>");
             }
             if (Math.round((1 - (1 / ((currentSong.notes.size() * 1000.0) / (currentSong.length * 10)))) * 3) <= 0) {
                         SongPlayer.addChatMessage("§6Not applying haste, as song is slow tempo");
